8d4dc25be0969c83404a899ce2e056bd62a3181f
Damian Mercado <damianmercado32@gmail.com>
2021-06-08 15:09:27 +0000

    learning about dictionaries

---
+++ classchallange/shebang12challange.py
@@ -0,0 +1,19 @@
+import random
+
+icecream= ["flavors", "salty"] 
+
+tlgclass= ["Brian","Clint","Damian","Dan","David","Jelani","Jerad","Jon","Jun","Mark","Max"]
+
+icecream.append("99")
+
+
+inputNumber = int(input("Enter a number from 0 -10 inclusive/n>"))
+
+print(f"99 flavors and {tlgclass[inputNumber]} chose to be salty")
+
+
+randomInt = random.randint(0, 10)
+print(f"99 flavors and {tlgclass[randomInt]} chose to be salty")
+
+
+
